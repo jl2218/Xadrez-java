43a07846db8329da2c8e6727cf662ca47c3e1a20
jl2218 <joaopedropepe56@gmail.com>
2023-06-26 17:02:11 -0300

    Lógica do Check

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -16,6 +16,11 @@ public abstract class ChessPiece extends Piece{
 	public Color getColor() {
 		return color;
 	}
+	
+	public ChessPosition getPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+	
 	public boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
