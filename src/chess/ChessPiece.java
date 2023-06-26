a527523d548007941fe9b54dc44e911ccee5b4ee
jl2218 <joaopedropepe56@gmail.com>
2023-06-26 17:56:43 -0300

    CheckMate implementado

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -17,7 +17,7 @@ public abstract class ChessPiece extends Piece{
 		return color;
 	}
 	
-	public ChessPosition getPosition() {
+	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
 	
