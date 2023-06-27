9fa12f3cd277ceffde16e1b61a0c2585dad50abf
João Pedro de Oliveira Faria <joaopedropepe56@gmail.com>
2023-06-26 21:31:33 -0300

    Movimentação do Peão consertada

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -10,7 +10,7 @@ public class Pawn extends ChessPiece {
 	public Pawn(Board board, Color color) {
 		super(board, color);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "P";
@@ -30,7 +30,7 @@ public class Pawn extends ChessPiece {
 			p.setValues(position.getRow() - 2, position.getColumn());
 			Position p2 = new Position(position.getRow() - 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2)
-					&& !getBoard().thereIsAPiece(p2)) {
+					&& !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 			p.setValues(position.getRow() - 1, position.getColumn() - 1);
@@ -49,7 +49,7 @@ public class Pawn extends ChessPiece {
 			p.setValues(position.getRow() + 2, position.getColumn());
 			Position p2 = new Position(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2)
-					&& !getBoard().thereIsAPiece(p2)) {
+					&& !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 			p.setValues(position.getRow() + 1, position.getColumn() - 1);
